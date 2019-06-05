30a52b3c3c7efe1f6aa5b39be0057749a23ee1fb
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-05 16:47:10 -0700

    added logger middleware just for mvp but I'm using morgan

--- middleware.js
+++ middleware.js
@@ -3,6 +3,11 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const logger = (req, res, next) => {
+    console.log(`A ${req.method} request to '${req.url}'`);
+    next();
+} //I only wrote this for MVP but I ain't using it
+
 module.exports = server => {
     server.use(express.json()); //JSON derrrruuuloooo
     server.use(cors()); //cross origin requests
