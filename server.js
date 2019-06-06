c95602ba109fe921e581535d8e4f18cd30b692a6
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-06 02:48:22 -0700

    oops, remove the dots! brain fart x2

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const userRoute = require('./users/userRouter');
 
 configMiddleware(server);
 
-server.use('./api/posts', postRoute);
-server.use('./api/users', userRoute);
+server.use('/api/posts', postRoute);
+server.use('/api/users', userRoute);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Up and running!</h2>`)
