3f31481c88eeeb04fe6e6d468550265d15aabdb6
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-05 17:12:37 -0700

    much excite! wow! added get server responses

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,16 +1,39 @@
-const express = 'express';
-
+const express = require('express');
 const router = express.Router();
+const postDB = require('./postDb');
+const userDB = require('../users/userDb');
 
 router.get('/', (req, res) => {
-
+    postDB
+    .get()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        res.status(418).json({ //i'm a teapot, short and stout
+            success: false,
+            err
+        });
+    });
 });
 
 router.get('/:id', (req, res) => {
-
+    const {id} = req.params;
+    postDB
+    .get(id)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        res.status(404).json({
+            success: false,
+            err
+        });
+    });
 });
 
 router.delete('/:id', (req, res) => {
+
 
 });
 
